cc530d679ba5880881d373bc4cc6baad75b1a63f
nohjunh <nsh042777@naver.com>
2022-06-25 19:43:17 +0900

    2447

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
-test_number= int(input())
-count=0
-for num in range(1, test_number+1):
-    test_string = list(map(int, str(num)))
-    if num <100:
-        count+=1
-    else:
-        if (test_string[0]-test_string[1]) == (test_string[1]-test_string[2]):
-            count+=1
+def solve(num):
+    global map
 
-print(count)
+    if num==3:
+        map[0][:3] = map[2][:3] = ["*"]*3
+        map[1][:3] = ["*"," ","*"]
+        return
+    
+    a= num//3
+    solve(num//3)
+    for i in range(3):
+        for j in range(3):
+            if i==1 and j==1:
+                continue
+            for k in range(a):
+                map[a*i+k][a*j : a*(j+1)] = map[k][:a]
+                
+
+test_input= int(input())
+
+map= [[" " for _ in range(test_input)] for _ in range(test_input)]
+
+solve(test_input)
+
+for i in map:
+    print("".join(str(s) for s in i))
