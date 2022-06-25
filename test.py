21b5a08f565cec2b2cba031ef92788a888722edd
nohjunh <nsh042777@naver.com>
2022-06-25 20:15:42 +0900

    2675

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-import string
+test_input= int(input())
+test_set=[]
+for i in range(test_input):
+    a,b = input().split()
+    test=""
+    for j in b:
+        for k in range(int(a)):
+           test+=j
+    test_set.append(test)
 
-test_input= list(input())
-
-for i in string.ascii_lowercase:
-    x= i not in test_input
-    if x:
-        print("-1", end=' ')
-    else:
-        print(test_input.index(i), end=' ')
+for i in test_set:
+    print(i)
