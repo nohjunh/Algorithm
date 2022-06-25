9d852a3150a1efb6abf11db91af92e13ce991025
nohjunh <nsh042777@naver.com>
2022-06-25 22:37:49 +0900

    1057

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
-count, max = map(int, input().split())
-test_input=list(map(int, input().split()))
-test_set=[]
-for i in range(count):
-    for j in range(i+1, count):
-        for k in range(j+1, count):
-            sum=test_input[i]+test_input[j]+test_input[k]
-            if sum>max:
-                continue
-            else:
-                test_set.append(sum)
+candidate, kim, lim = map(int, input().split())
+count=0
+while kim != lim:
+    if kim %2 ==0:
+        if kim//2 !=0:
+            kim//=2
+    else:
+        if kim//2 !=0:
+            kim//=2
+            kim+=1
 
-test_set.sort(reverse=True)
-print(test_set[0])
+    if lim %2 ==0:
+        if lim//2 !=0:
+            lim//=2
+    else:
+        if lim//2 !=0:
+            lim//=2
+            lim+=1
+    count+=1
+
+print(count)
