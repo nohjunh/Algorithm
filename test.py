82fc9359c4815c0bdc4849c17e1cb1abb026e37b
nohjunh <nsh042777@naver.com>
2022-06-25 20:04:40 +0900

    10809

--- test.py
+++ test.py
@@ -1,26 +1,10 @@
-def solve(num):
-    global map
+import string
 
-    if num==3:
-        map[0][:3] = map[2][:3] = ["*"]*3
-        map[1][:3] = ["*"," ","*"]
-        return
-    
-    a= num//3
-    solve(num//3)
-    for i in range(3):
-        for j in range(3):
-            if i==1 and j==1:
-                continue
-            for k in range(a):
-                map[a*i+k][a*j : a*(j+1)] = map[k][:a]
-                
+test_input= list(input())
 
-test_input= int(input())
-
-map= [[" " for _ in range(test_input)] for _ in range(test_input)]
-
-solve(test_input)
-
-for i in map:
-    print("".join(str(s) for s in i))
+for i in string.ascii_lowercase:
+    x= i not in test_input
+    if x:
+        print("-1", end=' ')
+    else:
+        print(test_input.index(i), end=' ')
