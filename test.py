ca43fb4ac9d54427545cedcec7fc1ce66560e70f
nohjunh <nsh042777@naver.com>
2022-06-25 21:14:30 +0900

    11659

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-a,b = map(int, input().split())
-test_list=list(map(int, input().split()))
+import sys
+
+a,b = map(int, sys.stdin.readline().split())
+test_list=list(map(int, sys.stdin.readline().split()))
 temp = [0]
 sum=0
 for i in test_list:
     sum+=i
     temp.append(sum)
 
-print(temp)
 for i in range(b):
-    _from, _to= map(int, input().split())
+    _from, _to= map(int, sys.stdin.readline().split())
     print(temp[_to] - temp[_from-1])
