7befa450f67d524c0f7029e5b6e36e15fd5ad356
nohjunh <nsh042777@naver.com>
2022-06-28 17:53:10 +0900

    2869

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-test_input= int(input())
-test_list= []
-for i in range(test_input):
-    A,B = map(str, input().split())
-    A= int(A)
-    test_list.append([A,B])
+import math
+A, B, V= map(int, input().split())
 
-test_list.sort(key=lambda x:x[0])
-
-for i in range(test_input):
-    print(test_list[i][0], test_list[i][1])
+day= (V-A) / (A-B)
+print(math.ceil(day) + 1)
