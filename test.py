8aa8d9d5b572a6efaf1b4bbdc162f860ca23452d
nohjunh <nsh042777@naver.com>
2022-07-10 13:13:12 +0900

    1085

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-N = int(input())
-A = set(map(int, input().split()))
-M = int(input())
-B = list(map(int, input().split()))
+x,y,w,h= map(int,input().split())
 
-# A를 set으로 하면 순서를 신경 안쓰므로 시간복잡도 성능 개선
-for i in B:
-    if i in A:
-        print("1")
-    else:
-        print("0")
- 
-
-
-    
-
-
+test1= w-x
+test2= h-y
+print(min(test1, test2, x, y))
