2d6d9dbafc5d603dd3ebcdbd6a6d7bf04b345d5e
nohjunh <nsh042777@naver.com>
2022-07-18 20:57:34 +0900

    4153

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-x,y,w,h= map(int,input().split())
-
-test1= w-x
-test2= h-y
-print(min(test1, test2, x, y))
+while(1):
+    a = list(map(int, input().split()))
+    max_value= max(a)
+    if max_value==0:
+        break
+    a.remove(max_value)
+    if (a[0]**2+a[1]**2) != max_value**2:
+      print("wrong")
+    else:
+      print("right")
