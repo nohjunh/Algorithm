67755fc704d2fd9258d46d685ee6d743c32f0994
nohjunh <nsh042777@naver.com>
2022-07-20 21:08:37 +0900

    24416

--- test.py
+++ test.py
@@ -1,10 +1,30 @@
-while(1):
-    a = list(map(int, input().split()))
-    max_value= max(a)
-    if max_value==0:
-        break
-    a.remove(max_value)
-    if (a[0]**2+a[1]**2) != max_value**2:
-      print("wrong")
+def fib(n):
+  if n==1 or n==2:
+    global count_code1
+    count_code1+=1
+    return 1
+  else:
+    return fib(n-1) + fib(n-2)
+  
+
+def fibonacci(n):
+  DP[1] = DP[2] = 1
+  for i in range(3, n+1):
+    global count_code2
+    count_code2+=1
+    if DP[i]==0:
+      DP[i]= DP[i-1] + DP[i-2]
     else:
-      print("right")
+      return DP[i]
+  return DP[n]
+
+DP= [0]*100
+n= int(input())
+global count_code1
+count_code1=0
+global count_code2
+count_code2=0
+fib(n)
+fibonacci(n)
+print(count_code1, count_code2)
+
