5f72a0b72754f35ba65dfd27ea0117006236fa1a
nohjunh <nsh042777@naver.com>
2022-07-20 21:24:34 +0900

    1932

--- test.py
+++ test.py
@@ -1,30 +1,18 @@
-def fib(n):
-  if n==1 or n==2:
-    global count_code1
-    count_code1+=1
-    return 1
-  else:
-    return fib(n-1) + fib(n-2)
-  
-
-def fibonacci(n):
-  DP[1] = DP[2] = 1
-  for i in range(3, n+1):
-    global count_code2
-    count_code2+=1
-    if DP[i]==0:
-      DP[i]= DP[i-1] + DP[i-2]
-    else:
-      return DP[i]
-  return DP[n]
-
-DP= [0]*100
 n= int(input())
-global count_code1
-count_code1=0
-global count_code2
-count_code2=0
-fib(n)
-fibonacci(n)
-print(count_code1, count_code2)
+DP = []
+for i in range(n):
+  DP.append(list(map(int,input().split())))
 
+row_element_count=2
+
+for i in range(1,n):
+  for num in range(row_element_count):
+    if num==0:
+      DP[i][num]= DP[i][num] + DP[i-1][num]
+    elif i==num:
+      DP[i][num]= DP[i][num] + DP[i-1][num-1]
+    else:
+      DP[i][num]= max(DP[i-1][num-1], DP[i-1][num])+DP[i][num]
+  row_element_count+=1
+
+print(max(DP[n-1]))
