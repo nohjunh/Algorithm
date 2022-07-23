dc917dbb71d2287928a4ed1ee89bca06c65c298e
nohjunh <nsh042777@naver.com>
2022-07-23 15:13:57 +0900

    11053

--- test.py
+++ test.py
@@ -1,24 +1,11 @@
-"""
-n=1 -> 1 1개
-n=2 -> 00, 11 2개
-n=3 -> 100 , 001, 111 3개
-n=4 -> 0000, 0011, 1100, 1001, 1111 5개
-n=5 -> 8개
-
-DP[n]= DP[n-1] + DP[n-2]
-"""
-
-def solve(N):
-  DP[1]=1
-  DP[2]=2
-  for i in range(3, N+1):
-    DP[i]= (DP[i-1] + DP[i-2]) % 15746
-  return DP[N]
-
 N= int(input())
-DP = [0 for _ in range(N+1)]
-if N==1 or N==2:
-  print(N%15746)
-else:
-  ans = solve(N)
-  print(ans)
+num_list= list(map(int, input().split()))
+
+DP = [1 for _ in range(N)]
+
+for i in range(N):
+  for j in range(i):
+    if num_list[j] < num_list[i]:
+      DP[i] = max(DP[i], DP[j]+1)
+
+print(max(DP))
