4bcb3a4cd67b3fbe2efbf348793fc9dae1831a07
nohjunh <nsh042777@naver.com>
2022-07-23 14:19:41 +0900

    1904

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ DP[n]= DP[n-1] + DP[n-2]
 """
 
 def solve(N):
+  DP[1]=1
+  DP[2]=2
   for i in range(3, N+1):
     DP[i]= (DP[i-1] + DP[i-2]) % 15746
   return DP[N]
 
 N= int(input())
 DP = [0 for _ in range(N+1)]
-DP[1]=1
-DP[2]=2
 if N==1 or N==2:
   print(N%15746)
 else:
