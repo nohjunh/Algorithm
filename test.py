ba786c2f92ba98a7936ed6b203b44d25ab7414b6
nohjunh <nsh042777@naver.com>
2022-07-23 21:55:01 +0900

    10828

--- test.py
+++ test.py
@@ -1,14 +1,31 @@
-A,B= map(int, input().split())
+import sys
 
-def gcd(x,y):
-  while y!=0:
-    x2=y
-    y= x%y
-    x=x2
-  return x
-
-def lcm(x,y):
-  return x*y // gcd(x,y)
-
-print(gcd(A,B))
-print(lcm(A,B))
+N=int(sys.stdin.readline())
+solve_list=[]
+count=0
+for i in range(N):
+  initial= list(map(str,sys.stdin.readline().split()))
+  Comm= initial[0]
+  if len(initial)==2:
+    value= initial[1]
+  if Comm=="push":
+    solve_list.append(int(value))
+    count+=1
+  elif Comm=="pop":
+    if count==0:
+      print("-1")
+    else:
+      count-=1
+      print(solve_list.pop())
+  elif Comm=="size":
+    print(len(solve_list))
+  elif Comm=="empty":
+    if count==0:
+      print("1")
+    else:
+      print("0")
+  elif Comm=="top":
+    if count==0:
+      print("-1")
+    else:
+      print(solve_list[count-1])
