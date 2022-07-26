a5b8932aac70b8e94073d58734b5e5cb44a09937
nohjunh <nsh042777@naver.com>
2022-07-26 14:04:20 +0900

    2740

--- test.py
+++ test.py
@@ -1,11 +1,28 @@
 import math
 import sys
 
-T= int(sys.stdin.readline())
-for _ in range(T):
-  H, W, N= map(int, sys.stdin.readline().split())
-  floor= N%H ## 층 수
-  count= math.ceil(N/H) ## 몇 번째 호수
-  if floor==0:
-    floor= H
-  print(floor*100+count)
+N, M= map(int, sys.stdin.readline().split())
+A_list= []
+for i in range(N):
+  A_list.append(list(map(int, sys.stdin.readline().split())))
+
+N, M= map(int, sys.stdin.readline().split())
+B_list= []
+for i in range(N):
+  B_list.append(list(map(int, sys.stdin.readline().split())))
+
+All_list= []
+
+for i in range(len(A_list)):
+  for j in range(M):
+    test=0
+    for k in range(len(A_list[i])):
+      test+= A_list[i][k]*B_list[k][j]
+    All_list.append(test)
+
+def list_chuck(arr, n):
+    return [arr[i: i + n] for i in range(0, len(arr), n)]
+
+list_chunked= list_chuck(All_list, 3)
+for i in list_chunked:
+  print(*i)
