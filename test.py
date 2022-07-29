efbe10103cf7bd6341281838b1263c4a66045cc4
nohjunh <nsh042777@naver.com>
2022-07-29 16:52:11 +0900

    Dijkstra_O(n**2)_case_test

--- test.py
+++ test.py
@@ -1,19 +1,49 @@
-import sys
-from collections import deque
+# 다익스트라 test
+# basic O(n**2)
 
-N, K = map(int, sys.stdin.readline().split())
-que= deque()
-ans= []
+import numpy as np
 
-for i in range(1, N+1):
-  que.append(i)
+INF= 1000000000
+number=6
 
-while(len(que)!=0):
-  for _ in range(K-1):
-    que.append(que[0])
-    que.popleft()
-  ans.append(que.popleft())
+a= np.array([
+  [0, 2, 5, 1, INF, INF],
+  [2, 0, 3, 2, INF, INF],
+  [5, 3, 0, 3, 1,5],
+  [1, 2, 3, 0, 1, INF],
+  [INF, INF, 1, 1, 0, 2],
+  [INF, INF, 5, INF, 2, 0]
+])
 
-print("<", end='')
-print(*ans, sep=', ',end='')
-print(">")
+# 방문한 노드 v배열
+v= [False for _ in range(number)]
+# 노드 거리 d배열
+d= [0 for _ in range(number)]
+
+def getSmallIndex():
+  min= INF
+  index= 0
+  for i in range(number):
+    if (d[i] < min) and not v[i]:
+      min = d[i]
+      index = i
+  return index
+
+def dijkstra(start):
+  for i in range(number):
+    d[i] = a[start][i]
+  
+  v[start]= True
+
+  for i in range(number-2):
+    current= getSmallIndex()
+    v[current]= True
+    for j in range(number):
+      if not v[j]:
+        if d[current] + a[current][j] < d[j]:
+          d[j] = d[current] + a[current][j]
+        
+
+dijkstra(0)
+for i in d:
+  print(i)
