39eb43aa8d4f6ef063fafcf99726c84b9ec0328f
nohjunh <nsh042777@naver.com>
2022-08-08 18:58:37 +0900

    24479: use_sys.setrecursionlimit

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
-# 1260
+# 24479
 import sys
 from collections import deque
 input= sys.stdin.readline
+sys.setrecursionlimit(100000)
 
-# N=정점의 개수, M=간선의 개수, V= 탐색할 정점의 번호
+# N=정점의 개수, M=간선의 개수, R= 탐색할 정점의 번호
 N, M, V = map(int, input().split())
 graph_test = [[] for _ in range(N+1)]
 
-# 양방향 연결
+# 무방향 연결 = 양방향 간선
 for i in range(M):
     node1, node2= map(int, input().split())
     graph_test[node1].append(node2)
@@ -15,33 +16,18 @@ for i in range(M):
 
 for i in range(len(graph_test)):
     graph_test[i].sort()
-        
+
+count=1
 def DFS_with_adj_list(graph_test, root):
-    visited.add(root)
-    print(root, end=' ')
-    if graph_test[root]==None:
-        return
+    global count
+    visited[root]=count
     for node in graph_test[root]:
-        if node not in visited:
+        if visited[node]==0:
+            count+=1
             DFS_with_adj_list(graph_test, node)
-    return visited
+    return
 
-def BFS_with_adj_list(graph_test, root):
-    queue= deque([root])
-    visited.add(root)
-    while queue:
-        n = queue.popleft()
-        print(n, end=' ')
-        for neigh in graph_test[n]:
-            if neigh not in visited:
-                visited.add(neigh)
-                queue.append(neigh)
-    return visited
-
-visited = set()
+visited = [0 for _ in range(N+1)]
 DFS_with_adj_list(graph_test, V)
-
-print()
-
-visited = set()
-BFS_with_adj_list(graph_test, V)
+for i in range(1,N+1):
+    print(visited[i])
