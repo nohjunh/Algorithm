28b113bc4f0c8e48f49d965d1848a7a484b411ea
nohjunh <nsh042777@naver.com>
2022-08-09 21:34:22 +0900

    24480

--- submit.py
+++ submit.py
@@ -1,41 +1,36 @@
-# 2178
+#24480
+from lib2to3.refactor import get_fixers_from_package
 import sys
-from collections import deque
-input= sys.stdin.readline
+input=sys.stdin.readline
+sys.setrecursionlimit(100000)
 
-# N= N개의 줄 , M= M개의 정수 로 (N,M)으로 미로
-N, M = map(int, input().split()) 
-graph_matrix = [[]*(M) for _ in range(N)]
-visited = [[0]*(M) for _ in range(N)] # visited이 0이면 아직 방문하지않은거
+# N개의 정점, M개의 간선, R=시작정점
+N, M, R= map(int, input().split())
 
-# 미로 정보 받아오기
-for i in range(N):
-    M_list= input().rstrip()
-    for j in range(len(M_list)):
-        graph_matrix[i].append(int(M_list[j]))
+graph_list = [[] for _ in range(N+1)]
 
-def BFS_with_adj_list(graph_matrix, first_x, first_y):
-    queue= deque( [(first_x, first_y)] ) # 큐 선언
-    visited[first_x][first_y]= 1 # 1,1부터 시작 -> 방문했다고 처리
-    while queue:
-        x, y = queue.popleft()
-        if x == N-1 and y==M-1:
-            print(visited[x][y])
-            break
+# 양방향 간선
+for i in range(M):
+    node1, node2 = map(int, input().split())
+    graph_list[node1].append(node2)
+    graph_list[node2].append(node1)
 
-        # 4방향을 검사
-        if (x+1 < N) and (visited[x+1][y] == 0) and (graph_matrix[x+1][y]== 1):
-            visited[x+1][y]= visited[x][y]+ 1 # 한칸 이동했으므로 이동횟수 +1
-            queue.append( (x+1, y) )
-        if (x-1 >= 0) and (visited[x-1][y] == 0) and (graph_matrix[x-1][y]== 1):
-            visited[x-1][y]= visited[x][y]+ 1
-            queue.append( (x-1, y) )
-        if ( y+1 < M) and (visited[x][y+1] == 0) and (graph_matrix[x][y+1]==1):
-            visited[x][y+1] = visited[x][y]+ 1
-            queue.append( (x, y+1) )
-        if( y-1 >= 0) and (visited[x][y-1] == 0) and (graph_matrix[x][y-1]== 1):
-            visited[x][y-1] = visited[x][y]+ 1
-            queue.append( (x, y-1) )   
+for i in range(1, N+1):
+    graph_list[i].sort(reverse=True)
 
-BFS_with_adj_list(graph_matrix, 0, 0)
+visited=[0 for _ in range(N+1)]
 
+count=1
+def DFS_adj_list(graph_list, root):
+    global count
+    visited[root]=count
+    for node in graph_list[root]:
+        if visited[node]==0:
+            count+=1
+            DFS_adj_list(graph_list, node)
+    return visited
+
+DFS_adj_list(graph_list, R)
+
+for i in range(1, N+1):
+    print(visited[i])
