1e46f458fa9323146127c5732b27da587c7d5a63
nohjunh <nsh042777@naver.com>
2022-08-09 19:50:05 +0900

    2178

--- grammar_test.py
+++ grammar_test.py
@@ -1,14 +1,6 @@
+arr = [[]]
+s = input()
+for i in range(len(s)):
+    arr[0].append(int(s[i]))
 
-graph= [ [] for _ in range(5+1)]
-a,b,c= map(int, input().split())
-
-graph[a].append( (b,c) )
-
-print(graph[a])
-for i in graph[a]:
-    print(i[0])
-
-import sys
-INF=sys.maxsize
-distance = [INF] * (5+1) # 먼저, start로부터의 각 노드별 최단 거리를 INF로 초기화
-print(distance)
+print(arr)
