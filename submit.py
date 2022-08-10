3dabe2daf6ac5e44b273438903b2e3ff76a3feff
nohjunh <nsh042777@naver.com>
2022-08-10 17:44:32 +0900

    2667

--- submit.py
+++ submit.py
@@ -1,37 +1,66 @@
-# 2606
+# 2667
 import sys
 input=sys.stdin.readline
 from collections import deque
-sys.setrecursionlimit(100000)
 
-# N= 컴퓨터의 수 
-# M= 컴퓨터 쌍의 수(간선)
-# R= 시작 컴퓨터는 1
+# N= 지도의 크기
 N=int(input())
-M=int(input())
 
-graph_list = [[] for _ in range(N+1)]
+graph_matrix = [[]*(N) for _ in range(N)]
 
-# 양방향 간선
-for i in range(M):
-    node1, node2 = map(int, input().split())
-    graph_list[node1].append(node2)
-    graph_list[node2].append(node1)
+# 지도 정보 받아오기
+for i in range(N):
+    N_list= input().rstrip()
+    for j in range(len(N_list)):
+        graph_matrix[i].append(int(N_list[j]))
 
-visited= set()
-count=0 #1번이 걸린거는 제외하고 생각! -> 1번에 의해 걸린 컴퓨터의 수를 세면 됨.
-def BFS_adj_list(graph_list, root):
-    global count
-    queue= deque([root])
-    visited.add(root)
-    while queue:
-        n= queue.popleft()
-        for neigh in graph_list[n]:
-            if neigh not in visited: # 여기서 vistied[neigh]==0 이라면 방문 안했다는거니까 방문해줘야 함.
-                count+=1
-                visited.add(neigh)
-                queue.append(neigh)
-    return visited
+visited= [[0]*(N) for _ in range(N)] # 0이면 아직 방문 안한거
 
-BFS_adj_list(graph_list, 1)
-print(count)
+def BFS_with_adj_list(graph_matrix, first_x, first_y):
+    home_count=1
+    queue= deque( [(first_x, first_y)] ) # 큐 선언
+    if graph_matrix[first_x][first_y]==1: # 1로 집이 있고
+        if visited[first_x][first_y]==0: # 방문하지 않았다면
+            visited[first_x][first_y]=1 # 방문처리
+            while queue:
+                x, y = queue.popleft()
+
+                # 4방향을 검사로 1인 방과 탐색이 가능한 방이면 같은 모집단이고 count를 증가시키며 방문처리함.
+                if (x+1 < N) and (visited[x+1][y] == 0) and (graph_matrix[x+1][y]== 1):
+                    visited[x+1][y]=1 #방문처리
+                    home_count+=1
+                    queue.append( (x+1, y) )
+                if (x-1 >= 0) and (visited[x-1][y] == 0) and (graph_matrix[x-1][y]== 1):
+                    visited[x-1][y]=1 #방문처리
+                    home_count+=1
+                    queue.append( (x-1, y) )
+                if ( y+1 < N) and (visited[x][y+1] == 0) and (graph_matrix[x][y+1]==1):
+                    visited[x][y+1]= 1 #방문처리
+                    home_count+=1
+                    queue.append( (x, y+1) )
+                if( y-1 >= 0) and (visited[x][y-1] == 0) and (graph_matrix[x][y-1]== 1):
+                    visited[x][y-1]= 1 #방문처리
+                    home_count+=1
+                    queue.append( (x, y-1) )
+            return home_count
+    return 0
+
+home_list=[]
+# N x N 지도를 돌면서 1인 방이 있으면 그 방부터 BFS탐색
+for i in range(N):
+    for j in range(N):
+        if graph_matrix[i][j]== 1:
+            home_list.append(BFS_with_adj_list(graph_matrix, i, j))
+
+home_list.sort()
+home_ans= []
+count_ans= 0
+# 모집단에 속한 것만 색출하기 위함.
+for i in range(len(home_list)):
+    if home_list[i] != 0:
+        count_ans += 1
+        home_ans.append(home_list[i])
+
+print(len(home_ans))
+for i in range(len(home_ans)):
+    print(home_ans[i])
