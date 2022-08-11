e610bb0a70802c1d08d57d23af93b49012f34609
nohjunh <nsh042777@naver.com>
2022-08-11 13:52:20 +0900

    7576

--- grammar_test.py
+++ grammar_test.py
@@ -1,16 +1,6 @@
-# 1012
-import sys
-input=sys.stdin.readline
-from collections import deque
-
-# T= 테스트 케이스의 개수
-T=int(input())
-
-# M=가로길이, N= 세로길이, K= 배추가 심어져 있는 위치의 개수
-M, N, K = map(int, input().split())
-graph_matrix = [[0]*(M) for _ in range(N)]
-print(graph_matrix)
-# 배추가 심어져 있는 땅 정보 받아오기
-for i in range(K):
-    x,y= map(int, input().split())
-    graph_matrix[9][6]= 1
+graph_matrix= []
+# 토마토 정보 받아오기
+for i in range(1):
+    N_list= input().rstrip()
+    for j in range(len(N_list)):
+        graph_matrix[i].append(int(N_list[j]))
--- submit.py
+++ submit.py
@@ -1,63 +1,50 @@
-# 1012
+# 7576
 import sys
 input=sys.stdin.readline
 from collections import deque
 
-# T= 테스트 케이스의 개수
-T=int(input())
+# M,N 상자의 크기
+# M= 상자의 가로 칸 수
+# N= 상자의 세로 칸 수
+M,N =map(int, input().split())
 
-for _ in range(T):
-    # M=가로길이, N= 세로길이, K= 배추가 심어져 있는 위치의 개수
-    M, N, K = map(int, input().split())
-    graph_matrix = [[0]*(M) for _ in range(N)]
-    # 배추가 심어져 있는 땅 정보 받아오기
-    for i in range(K):
-        x,y= map(int, input().split())
-        graph_matrix[y][x]= 1
+graph_matrix=[list(map(int, input().split())) for _ in range(N)]
 
-    visited= [[0]*(M) for _ in range(N)] # 0이면 아직 방문 안한거
-    def BFS_with_adj_list(graph_matrix, first_x, first_y):
-        earthworms_count= 1
-        queue= deque( [(first_x, first_y)] ) # 큐 선언
-        if graph_matrix[first_x][first_y]==1: # 1로 배추가 있고
-            if visited[first_x][first_y]==0: # 방문하지 않았다면
-                visited[first_x][first_y]=1 # 방문처리
-                while queue:
-                    x, y = queue.popleft()
+def BFS_adj_list(one_list):
+    queue= deque() # 큐 선언
+    for i in one_list:
+        queue.append(i)
+    while queue:
+        x, y = queue.popleft()
+        # 4방향을 검사
+        # 단 익은 Day는 -1을 해줘야 정확함. -> 첫 시작 토마토 값이 1부터 시작하기 때문
+        if (x+1 < N)  and (graph_matrix[x+1][y]== 0):
+            graph_matrix[x+1][y]= graph_matrix[x][y]+1 # 해당 좌표의 토마토가 익은 day
+            queue.append( (x+1, y) )
+        if (x-1 >= 0)  and (graph_matrix[x-1][y]== 0):
+            graph_matrix[x-1][y]= graph_matrix[x][y]+1 # 해당 좌표의 토마토가 익은 day
+            queue.append( (x-1, y) )
+        if ( y+1 < M) and (graph_matrix[x][y+1]==0):
+            graph_matrix[x][y+1]= graph_matrix[x][y]+1 # 해당 좌표의 토마토가 익은 day
+            queue.append( (x, y+1) )
+        if( y-1 >= 0) and (graph_matrix[x][y-1]== 0):
+            graph_matrix[x][y-1]= graph_matrix[x][y]+1 # 해당 좌표의 토마토가 익은 day
+            queue.append( (x, y-1) )
 
-                    # 4방향을 검사로 1인 땅과 탐색이 가능한 땅이면 같은 모집단이고 count를 증가시키며 방문처리함.
-                    if (x+1 < N) and (visited[x+1][y] == 0) and (graph_matrix[x+1][y]== 1):
-                        visited[x+1][y]=1 #방문처리
-                        earthworms_count+=1
-                        queue.append( (x+1, y) )
-                    if (x-1 >= 0) and (visited[x-1][y] == 0) and (graph_matrix[x-1][y]== 1):
-                        visited[x-1][y]=1 #방문처리
-                        earthworms_count+=1
-                        queue.append( (x-1, y) )
-                    if ( y+1 < M) and (visited[x][y+1] == 0) and (graph_matrix[x][y+1]==1):
-                        visited[x][y+1]= 1 #방문처리
-                        earthworms_count+=1
-                        queue.append( (x, y+1) )
-                    if( y-1 >= 0) and (visited[x][y-1] == 0) and (graph_matrix[x][y-1]== 1):
-                        visited[x][y-1]= 1 #방문처리
-                        earthworms_count+=1
-                        queue.append( (x, y-1) )
-                return earthworms_count
-        return 0
+one_list= []
+for i in range(N):
+    for j in range(M):
+        if graph_matrix[i][j]== 1:
+            one_list.append( (i,j) )
+BFS_adj_list(one_list)
+ans=0
+for i in range(N):
+    for j in range(M):
+        if graph_matrix[i][j]==0:
+            print(-1)
+            exit()
+        else:
+            ans= max(graph_matrix[i][j], ans)
 
-    ground_list=[]
-    # M*N 땅을 돌면서 1인 땅이 있으면 그 땅부터 BFS탐색
-    for i in range(N):
-        for j in range(M):
-            if graph_matrix[i][j]== 1:
-                ground_list.append(BFS_with_adj_list(graph_matrix, i, j))
-
-    ground_list.sort()
-    ground_ans= []
-    count_ans= 0
-    # 모집단에 속한 것만 색출하기 위함.
-    for i in range(len(ground_list)):
-        if ground_list[i] != 0:
-            count_ans += 1
-            ground_ans.append(ground_list[i])
-    print(count_ans)
+# 그래프의 시작은 1부터 시작하는 반면 Day는 0->1일 부터 번지는 것을 고려해서 -1을 해준다.
+print(ans-1)
