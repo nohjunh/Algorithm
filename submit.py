8cb94531ab09d3a0fdfc849e7b90e50e085e9138
nohjunh <nsh042777@naver.com>
2022-08-12 19:06:40 +0900

    1389

--- submit.py
+++ submit.py
@@ -1,21 +1,46 @@
-# 11403
+# 1389
 import sys
 input= sys.stdin.readline
+INF= int(1e9) # 무한
 
-# N= 정점(노드)의 갯수
-N= int(input())
+# N= 유저의 수,  M= 친구 관계의 수
+N,M= map(int, input().split())
+
 # 2차원 배열->그래프 만들고 모든 값을 무한으로 초기화
+graph_matrix=[[INF]*(N+1) for _ in range(N+1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        if i==j:
+            graph_matrix[i][j]=0
+
 # 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
-graph_matrix=[list(map(int, input().rstrip().replace(" ", ""))) for _ in range(N)]
+for _ in range(M):
+    # A와 B가 친구일때 1라고 설정
+    # B와 A도 친구
+    a,b= map(int, input().split())
+    graph_matrix[a][b]=1
+    graph_matrix[b][a]=1
 
 # 점화식에 따라 플로이드 와샬 알고리즘 수행
-for k in range(0, N): # 거쳐가는 정점
-    for i in range(0, N): # 시작 정점
-        for j in range(0, N): # 도착 정점
-            if graph_matrix[i][k]==1 and graph_matrix[k][j]==1:
-                graph_matrix[i][j]=1
+for k in range(1,N+1): # 거쳐가는 사람
+    for i in range(1,N+1): # 시작 사람
+        for j in range(1,N+1): # 도착 사람
+            if i==j:
+                continue
+            else:
+                graph_matrix[i][j]=min(graph_matrix[i][j], graph_matrix[i][k]+graph_matrix[k][j])
 
-for i in range(N):
-    for j in range(N):
-        print(graph_matrix[i][j], end=" ")
-    print()
+# 수행 결과 출력
+ans=10000000
+ans_person=0
+for i in range(1, N+1):
+    total=0
+    for j in range(1, N+1):
+      total+=graph_matrix[i][j]
+    if ans>total:
+        ans=total
+        ans_person=i
+
+print(ans_person)
