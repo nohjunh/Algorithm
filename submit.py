3c3e4ed7bc3483abac7e64adce9768c33bc6e0d5
nohjunh <nsh042777@naver.com>
2022-08-12 12:40:07 +0900

    16928

--- submit.py
+++ submit.py
@@ -1,37 +1,48 @@
-# 1697
+# 16928
+from shutil import move
 import sys
 input=sys.stdin.readline
 from collections import deque
 
-# N= 수빈이의 현재 위치
-# K= 동생의 현재 위치
-N, K = map(int, input().split())
+# 보드판은 1~100까지의 수가 하나씩 적혀있는 10X10
+board=[0]*101
 
-max_value= 100000
-graph_matrix=[0 for _ in range(max_value+1)]
-graph_matrix[N]= 1 # 수빈이의 현재 위치 check
-ans_list=[]
+# N=사다리의 수, M= 뱀의 수
+N, M= map(int, input().split())
 
-def BFS_adj_list(graph_matrix, N):
-    queue = deque()
-    queue.append( N )
+# 사다리와 뱀은 해당 좌표에 도착할 시 위치가 미리 저장된 곳으로 워프해야되므로
+# 좌표 값을 쌍으로 저장하고 있어야 된다. -> 딕셔너리 이용
+ladder= {}
+snake= {}
+for _ in range(N):
+    a, b =map(int,input().split())
+    ladder[a]=b
+for _ in range(M):
+    a, b= map(int, input().split())
+    snake[a]=b
+
+visited=[0 for _ in range(101)]
+
+def BFS_adj_list(board):
+    # 1번부터 시작 (문제조건)
+    queue= deque([1])
     while queue:
-        N = queue.popleft()
+        position = queue.popleft()
+        if position==100: # 100번째 칸에 도착 -> 주사위를 몇 번 굴러야하는지 출력
+            return board[100]
+        # 주사위 값 1~6
+        for value in range(1,7):
+            move_value= position+value
+            if 1<=move_value<=100 and visited[move_value]==0:
+                # ladder딕셔너리의 key값들 중에 move_value가 있다면
+                if move_value in ladder.keys():
+                    move_value= ladder[move_value]
+                # snake딕셔너리의 key값들 중에 move_value가 있다면
+                if move_value in snake.keys():
+                    move_value= snake[move_value]
+                if visited[move_value]==0:
+                    visited[move_value]= 1
+                    board[move_value]= board[position]+1
+                    queue.append(move_value)
 
-        # 방향을 검사
-        if N==K: #수빈이가 동생 위치로 갔다면
-            global ans_list
-            ans_list.append(graph_matrix[N])
-        
-        if (0<= N+1 <= max_value) and (graph_matrix[N+1]==0):
-            graph_matrix[N+1]= graph_matrix[N]+1
-            queue.append( N+1 )
-        if (0<= N-1 <= max_value) and (graph_matrix[N-1]==0):
-            graph_matrix[N-1]= graph_matrix[N]+1 
-            queue.append( N-1 )
-        if (0<= 2*N <= max_value) and (graph_matrix[2*N]==0):
-            graph_matrix[2*N]= graph_matrix[N]+1 
-            queue.append( 2*N )
-
-BFS_adj_list(graph_matrix, N)
-print(min(ans_list)-1)
+print(BFS_adj_list(board))
--- test.py
+++ test.py
@@ -1,33 +1,30 @@
-# 24479
-import sys
-from collections import deque
-input= sys.stdin.readline
-sys.setrecursionlimit(100000)
 
-# N=정점의 개수, M=간선의 개수, R= 탐색할 정점의 번호
-N, M, V = map(int, input().split())
-graph_test = [[] for _ in range(N+1)]
 
-# 무방향 연결 = 양방향 간선
-for i in range(M):
-    node1, node2= map(int, input().split())
-    graph_test[node1].append(node2)
-    graph_test[node2].append(node1)
+max_value= 100000
+graph_matrix=[0 for _ in range(max_value+1)]
+graph_matrix[N]= 1 # 수빈이의 현재 위치 check
+ans_list=[]
 
-for i in range(len(graph_test)):
-    graph_test[i].sort()
+def BFS_adj_list(graph_matrix, N):
+    queue = deque()
+    queue.append( N )
+    while queue:
+        N = queue.popleft()
 
-count=1
-def DFS_with_adj_list(graph_test, root):
-    global count
-    visited[root]=count
-    for node in graph_test[root]:
-        if visited[node]==0:
-            count+=1
-            DFS_with_adj_list(graph_test, node)
-    return
+        # 방향을 검사
+        if N==K: #수빈이가 동생 위치로 갔다면
+            global ans_list
+            ans_list.append(graph_matrix[N])
+        
+        if (0<= N+1 <= max_value) and (graph_matrix[N+1]==0):
+            graph_matrix[N+1]= graph_matrix[N]+1
+            queue.append( N+1 )
+        if (0<= N-1 <= max_value) and (graph_matrix[N-1]==0):
+            graph_matrix[N-1]= graph_matrix[N]+1 
+            queue.append( N-1 )
+        if (0<= 2*N <= max_value) and (graph_matrix[2*N]==0):
+            graph_matrix[2*N]= graph_matrix[N]+1 
+            queue.append( 2*N )
 
-visited = [0 for _ in range(N+1)]
-DFS_with_adj_list(graph_test, V)
-for i in range(1,N+1):
-    print(visited[i])
+BFS_adj_list(graph_matrix, N)
+print(min(ans_list)-1)
