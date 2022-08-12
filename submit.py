9480eacebdc4b650a35110d632b98d7c0cd0de62
nohjunh <nsh042777@naver.com>
2022-08-12 18:47:54 +0900

    11403

--- submit.py
+++ submit.py
@@ -1,41 +1,21 @@
-# Floyd-Warshall 알고리즘 test
-from lib2to3.refactor import get_fixers_from_package
+# 11403
 import sys
 input= sys.stdin.readline
-INF= int(1e9) # 무한
 
-# N= 노드의 갯수,  M= 간선의 갯수
+# N= 정점(노드)의 갯수
 N= int(input())
-M= int(input())
 # 2차원 배열->그래프 만들고 모든 값을 무한으로 초기화
-graph_matrix=[[INF]*(N+1) for _ in range(N+1)] # 0번 인덱스는 사용하지 않음
-
-# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        if i==j:
-            graph_matrix[i][j]=0
-
 # 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
-for _ in range(M):
-    # A에서 B로 가는 비용은 C라고 설정
-    a,b,c = map(int, input().split())
-    graph_matrix[a][b]=c
+graph_matrix=[list(map(int, input().rstrip().replace(" ", ""))) for _ in range(N)]
 
 # 점화식에 따라 플로이드 와샬 알고리즘 수행
-for k in range(1, N+1): # 거쳐가는 정점
-    for i in range(1, N+1): # 시작 정점
-        for j in range(1, N+1): # 도착 정점
-            # i에서 j로 바로 가는 것보다 i에서 k + k에서 j로 가는 값 중 더 작은 값을 i에서 j로 가는 경로의 값으로 설정
-            graph_matrix[i][j]= min(graph_matrix[i][j], graph_matrix[i][k]+graph_matrix[k][j])
+for k in range(0, N): # 거쳐가는 정점
+    for i in range(0, N): # 시작 정점
+        for j in range(0, N): # 도착 정점
+            if graph_matrix[i][k]==1 and graph_matrix[k][j]==1:
+                graph_matrix[i][j]=1
 
-# 수행 결과 출력
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        # 도달할 수 없는 것들은 INF로 출력
-        if graph_matrix[i][j]==INF:
-            print("INF", end=' ')
-        else:
-            print(graph_matrix[i][j], end=' ')
-
-print()
+for i in range(N):
+    for j in range(N):
+        print(graph_matrix[i][j], end=" ")
+    print()
--- test.py
+++ test.py
@@ -1,30 +1,40 @@
+# Floyd-Warshall 알고리즘 test
+import sys
+input= sys.stdin.readline
+INF= int(1e9) # 무한
 
+# N= 노드의 갯수,  M= 간선의 갯수
+N= int(input())
+M= int(input())
+# 2차원 배열->그래프 만들고 모든 값을 무한으로 초기화
+graph_matrix=[[INF]*(N+1) for _ in range(N+1)] # 0번 인덱스는 사용하지 않음
 
-max_value= 100000
-graph_matrix=[0 for _ in range(max_value+1)]
-graph_matrix[N]= 1 # 수빈이의 현재 위치 check
-ans_list=[]
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if i==j:
+            graph_matrix[i][j]=0
 
-def BFS_adj_list(graph_matrix, N):
-    queue = deque()
-    queue.append( N )
-    while queue:
-        N = queue.popleft()
+# 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+for _ in range(M):
+    # A에서 B로 가는 비용은 C라고 설정
+    a,b,c = map(int, input().split())
+    graph_matrix[a][b]=c
 
-        # 방향을 검사
-        if N==K: #수빈이가 동생 위치로 갔다면
-            global ans_list
-            ans_list.append(graph_matrix[N])
-        
-        if (0<= N+1 <= max_value) and (graph_matrix[N+1]==0):
-            graph_matrix[N+1]= graph_matrix[N]+1
-            queue.append( N+1 )
-        if (0<= N-1 <= max_value) and (graph_matrix[N-1]==0):
-            graph_matrix[N-1]= graph_matrix[N]+1 
-            queue.append( N-1 )
-        if (0<= 2*N <= max_value) and (graph_matrix[2*N]==0):
-            graph_matrix[2*N]= graph_matrix[N]+1 
-            queue.append( 2*N )
+# 점화식에 따라 플로이드 와샬 알고리즘 수행
+for k in range(1, N+1): # 거쳐가는 정점
+    for i in range(1, N+1): # 시작 정점
+        for j in range(1, N+1): # 도착 정점
+            # i에서 j로 바로 가는 것보다 i에서 k + k에서 j로 가는 값 중 더 작은 값을 i에서 j로 가는 경로의 값으로 설정
+            graph_matrix[i][j]= min(graph_matrix[i][j], graph_matrix[i][k]+graph_matrix[k][j])
 
-BFS_adj_list(graph_matrix, N)
-print(min(ans_list)-1)
+# 수행 결과 출력
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        # 도달할 수 없는 것들은 INF로 출력
+        if graph_matrix[i][j]==INF:
+            print("INF", end=' ')
+        else:
+            print(graph_matrix[i][j], end=' ')
+
+print()
