95110ba4a79182a3d705f343e0bd5126ec7c172a
nohjunh <nsh042777@naver.com>
2022-08-15 00:35:43 +0900

    14938

--- submit.py
+++ submit.py
@@ -1,24 +1,27 @@
-# 2660 회장뽑기
+# 14938 서강그라운드
 from lib2to3.refactor import get_fixers_from_package
 import sys
 input= sys.stdin.readline
 INF= int(1e9) # 무한
 
-# N= 회원의 수 
-N= int(input())
+# N= 지역의 개수,  M= 수색범위, R= 길의 갯수
+N,M,R= map(int, input().split())
 # 2차원 배열->그래프 만들고 모든 값을 무한으로 초기화
 graph_matrix=[[INF]*(N) for _ in range(N)] # 0번 인덱스는 사용하지 않음
 
+# 각 지역에 있는 아이템의 수
+item_value=[0 for _ in range(N)]
+value= list(map(int,input().split()))
+for i in range(N):
+    item_value[i]=value[i]
+
 # 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
-while(True):
-    # 회원번호 A,B는 서로 친구 관계
-    a,b= map(int, input().split())
-    if a==-1 and b==-1:
-        break
-    # a와 b가 친구 사이면 1을 줌.
-    graph_matrix[a-1][b-1]=1
-    # b와 a도 친구 사이
-    graph_matrix[b-1][a-1]=1
+for _ in range(R):
+    # A에서 B로 가는 비용은 C라고 설정
+    a,b,l = map(int, input().split())
+    # 양방향 통행이 가능
+    graph_matrix[a-1][b-1]=l
+    graph_matrix[b-1][a-1]=l
 
 # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
 for i in range(N):
@@ -30,17 +33,15 @@ for i in range(N):
 for k in range(N): # 거쳐가는 정점
     for i in range(N): # 시작 정점
         for j in range(N): # 도착 정점
-            graph_matrix[i][j]= min(graph_matrix[i][j], graph_matrix[i][k]+graph_matrix[k][j])
+                graph_matrix[i][j]= min(graph_matrix[i][j], graph_matrix[i][k]+graph_matrix[k][j])
 
-level=[]
-# 각 회원 별 점수 중 최댓값 찾기
+ans_list=[]
 for i in range(N):
-    level.append(max(graph_matrix[i]))
-min_value=min(level)
-# 회장 후보의 점수와 후보의 수를 출력
-print(min_value, level.count(min_value))
-# 회장 후보를 오름차순으로 모두 출력
-# index를 0부터 시작했으므로 i+1로 출력한다. 그래야 1번회원부터의 탐색결과가 나옴
-for i in range(N):
-    if level[i]==min_value:
-        print(i+1, end=' ')
+    ans=0
+    for j in range(N):
+        if graph_matrix[i][j]<=M:
+            ans+=item_value[j]
+    ans_list.append(ans)
+
+# 예은이가 얻을 수 있는 최대 아이템 개수를 출력
+print(max(ans_list))
