53d6aae063a13d1da8f467192b8af8a1213874d0
nohjunh <nsh042777@naver.com>
2022-08-15 15:53:15 +0900

    BFS,DFS test 1260번 기준

--- submit.py
+++ submit.py
@@ -1,19 +1,46 @@
-# 11286 절댓값 힙
+# 1260
 import sys
-import heapq
+from collections import deque
 input= sys.stdin.readline
+# N=정점의 개수, M=간선의 개수, V= 탐색할 정점의 번호
+N, M, V = map(int, input().split())
+graph_test = [[] for _ in range(N+1)]
 
-N= int(input())
-heap=[]
-for _ in range(N):
-    x= int(input().rstrip())
-    if x!=0:
-        # 힙에 넣을 때 절댓값과 기존 값을 튜플형태로 묶어서 넣는다.
-        heapq.heappush(heap, (abs(x), x))
-    if x==0:
-        if len(heap)!=0:
-            # 실제 값은 튜플의 두번 째 자리에 저장되어 있으므로
-            # [1]인덱싱으로 접근한다.
-            print(heapq.heappop(heap)[1])
-        else:
-            print("0")
+# 양방향 연결
+for i in range(M):
+    node1, node2= map(int, input().split())
+    graph_test[node1].append(node2)
+    graph_test[node2].append(node1)
+
+for i in range(len(graph_test)):
+    graph_test[i].sort()
+
+def DFS_with_adj_list(graph_test, root):
+    visited.add(root)
+    print(root, end=' ')
+    if graph_test[root]==None:
+        return
+    for node in graph_test[root]:
+        if node not in visited:
+            DFS_with_adj_list(graph_test, node)
+    return visited
+
+def BFS_with_adj_list(graph_test, root):
+    queue= deque([root])
+    visited.add(root)
+    while queue:
+        n = queue.popleft()
+        print(n, end=' ')
+        for neigh in graph_test[n]:
+            if neigh not in visited:
+                visited.add(neigh)
+                queue.append(neigh)
+    return visited
+
+visited = set()
+DFS_with_adj_list(graph_test, V)
+
+print()
+
+visited = set()
+BFS_with_adj_list(graph_test, V) 
