9924cd7391e0146771784cda6444f43ed41e57b0
nohjunh <nsh042777@naver.com>
2022-08-15 16:20:00 +0900

    11724 연결요소의 개수

--- submit.py
+++ submit.py
@@ -1,46 +1,33 @@
-# 1260
+# 11724 연결 요소의 개수
 import sys
 from collections import deque
 input= sys.stdin.readline
-# N=정점의 개수, M=간선의 개수, V= 탐색할 정점의 번호
-N, M, V = map(int, input().split())
+# N=정점의 개수, M=간선의 개수
+N, M= map(int, input().split())
+# 노드 번호가 1부터 주어지므로 N+1
 graph_test = [[] for _ in range(N+1)]
+visited= [False]*(N+1)
 
-# 양방향 연결
+# 무방향이므로 양방향 연결
 for i in range(M):
     node1, node2= map(int, input().split())
     graph_test[node1].append(node2)
     graph_test[node2].append(node1)
 
-for i in range(len(graph_test)):
-    graph_test[i].sort()
-
-def DFS_with_adj_list(graph_test, root):
-    visited.add(root)
-    print(root, end=' ')
-    if graph_test[root]==None:
-        return
-    for node in graph_test[root]:
-        if node not in visited:
-            DFS_with_adj_list(graph_test, node)
-    return visited
-
 def BFS_with_adj_list(graph_test, root):
     queue= deque([root])
-    visited.add(root)
+    visited[root]=True
     while queue:
         n = queue.popleft()
-        print(n, end=' ')
         for neigh in graph_test[n]:
-            if neigh not in visited:
-                visited.add(neigh)
+            if visited[neigh]==False:
+                visited[neigh]=True
                 queue.append(neigh)
-    return visited
 
-visited = set()
-DFS_with_adj_list(graph_test, V)
+count=0
+for i in range(1,N+1):
+    if not visited[i]:
+        BFS_with_adj_list(graph_test, i)
+        count+=1
 
-print()
-
-visited = set()
-BFS_with_adj_list(graph_test, V) 
+print(count)
