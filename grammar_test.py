9806c9839c0e9a4983491be2df9deea931cfa1e3
nohjunh <nsh042777@naver.com>
2022-08-16 14:23:04 +0900

    14502 연구소

--- grammar_test.py
+++ grammar_test.py
@@ -1,11 +1,45 @@
 import sys
-import heapq
 
-N= int(input())
-heap=[]
+N = int(sys.stdin.readline())
+Nlist = []
+often = []
 
-for _ in range(2):
-    x= int(input().rstrip())
-    if x!=0:
-        # 힙에 넣을 때 절댓값과 기존 값을 묶어서 넣는다.
-        heapq.heappush(heap, (abs(x), x))
+for i in range(N):
+    Nlist.append(int(sys.stdin.readline()))
+
+print(round(sum(Nlist)/N)) #평균
+
+Nlist.sort()
+
+print(Nlist[int((N-1)/2)]) #중앙값
+
+hashmap = {}
+big = Nlist[0]
+cnt = 0
+
+for i in Nlist:
+    if i in hashmap:
+        hashmap[i] += 1
+    else:
+        hashmap[i] = 1
+
+for i in range(-4000, 4001):
+    if i in hashmap:
+        hashmap[big] < hashmap[i]
+        big = i
+
+for i in range(-4000, 4001):
+    if i in hashmap:
+        if hashmap[big] == hashmap[i]:
+            often.append(i)
+            cnt+=1
+
+often.sort()
+print("~~~",  big)
+#최빈값
+if cnt==1:
+    print(big)
+else:
+    print(often[0])
+
+print(max(Nlist)-min(Nlist)) # 범위
--- submit.py
+++ submit.py
@@ -1,33 +1,70 @@
-# 11724 연결 요소의 개수
+# 14502 연구소
 import sys
 from collections import deque
+import copy
 input= sys.stdin.readline
-# N=정점의 개수, M=간선의 개수
+# N= 연구소의 세로 크기
+# M= 연구소의 가로 크기
 N, M= map(int, input().split())
-# 노드 번호가 1부터 주어지므로 N+1
-graph_test = [[] for _ in range(N+1)]
-visited= [False]*(N+1)
+graph_matirx = [list(map(int, input().split())) for _ in range(N)]
 
-# 무방향이므로 양방향 연결
-for i in range(M):
-    node1, node2= map(int, input().split())
-    graph_test[node1].append(node2)
-    graph_test[node2].append(node1)
+# 4방향 탐색
+dx= [0, 0, 1, -1]
+dy= [1, -1, 0, 0]
 
-def BFS_with_adj_list(graph_test, root):
-    queue= deque([root])
-    visited[root]=True
+def BFS_adj_list():
+    queue= deque()
+    # matrix 복사
+    wall_graph_matrix= copy.deepcopy(graph_matirx)
+    for i in range(N):
+        for j in range(M):
+            if wall_graph_matrix[i][j]==2:
+                queue.append( (i,j) )
     while queue:
-        n = queue.popleft()
-        for neigh in graph_test[n]:
-            if visited[neigh]==False:
-                visited[neigh]=True
-                queue.append(neigh)
+        x,y = queue.popleft()
+        # 4방향 검사
+        for i in range(4):
+            nx= x+dx[i]
+            ny= y+dy[i]
+            # 연구소 지도 크기 범위 검사
+            # nx가 2차원 배열에 1요소이므로 세로크기를 나타냄
+            # [nx][ny] => [세로][가로] => N=세로, M=가로
+            if 0<= nx < N and 0<= ny< M:
+                if wall_graph_matrix[nx][ny]==0:
+                    wall_graph_matrix[nx][ny]=2
+                    queue.append((nx,ny))
+    zero_count=0
+    for i in range(N):
+        for j in range(M):
+            if wall_graph_matrix[i][j]==0:
+                zero_count+=1
+    ans_list.append(zero_count)
 
-count=0
-for i in range(1,N+1):
-    if not visited[i]:
-        BFS_with_adj_list(graph_test, i)
-        count+=1
 
-print(count)
+def wall(wall_count):
+    # 재귀를 통해 빈 공간을 탐색하는 것은
+    # 직접 패드로 인덱스 따라 그려보며 판단하기.
+    if wall_count==3:
+        BFS_adj_list()
+        return
+    else:
+        for i in range(N):
+            for j in range(M):
+               if graph_matirx[i][j]==0:
+                    # 길에 벽을 만듬
+                    graph_matirx[i][j]=1
+                    # wall_count는 재귀별로 고정일 것임.
+                    # 첫번 째 재귀에서는 count=1이고
+                    # 두번 째 재귀에서는 count=2
+                    # 세번 째 재귀에서는 count=3
+                    # 세번 째 재귀가 끝나면 count=2가 되니 
+                    # 두번 째 벽의 값을 바꾸고 다시 세번 째 재귀문 호출
+                    # 반복
+                    wall(wall_count+1)
+                    graph_matirx[i][j]=0
+                    
+
+    
+ans_list=[]
+wall(0)
+print(max(ans_list))
