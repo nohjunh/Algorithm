3eb5d78c4d59648a33b6e44fdd645e2f84c1b098
nohjunh <nsh042777@naver.com>
2022-08-18 13:37:03 +0900

    이항계수

--- grammar_test.py
+++ grammar_test.py
@@ -1,15 +1,12 @@
 import sys
 input= sys.stdin.readline
 
-K=int(input())
-stack=[]
-for i in range(K):
-  test= int(input())
-  if test==0:
-    stack.pop()
+def facto(num):
+  if num<=1:
+    return 1
   else:
-    stack.append(test)
-ans=0
-for i in range(len(stack)):
-  ans+=stack[i]
-print(ans)
+    return num*facto(num-1)
+
+# 이항계수: n!/(n-k)!k!
+N, K= map(int, input().split())
+print(int(facto(N)/(facto(N-K)*facto(K))))
