c02db7611a4b1e6625b64355ff10da5400ecaea6
nohjunh <nsh042777@naver.com>
2022-08-20 15:10:27 +0900

    10026 적록색약

--- grammar_test.py
+++ grammar_test.py
@@ -1,55 +1,101 @@
-# 2583 영역 구하기
+# 10026 적록색약 
 import sys
 from collections import deque
 input= sys.stdin.readline
-# M= 모눈종이의 세로 크기
-# N= 모눈종이의 가로 크기
-# K= 모눈종이 위에 K개의 직사각형
-M,N,K= map(int, input().split())
-graph_matrix = [[0]*(N) for _ in range(M)]
+
+# N= 그리드 크기 
+N= int(input())
+graph_matrix = [list(input().rstrip()) for _ in range(N)]
 
 # 4방향검사
 dx=[0,0,1,-1]
 dy=[1,-1,0,0]
 
-# ex) (0,2), (4,4)
-# ex) 4-0, 4-2 = 4(가로길이), 2(세로길이)
-for _ in range(K):
-  left_x, left_y, right_x, right_y= map(int, input().split())
-  for i in range(left_y, right_y): #세로길이 범위
-    for j in range(left_x, right_x): #가로길이 범위
-      graph_matrix[(M-1)-i][j]=1
-
-
-def BFS_adj_list(graph_matrix, first_x, first_y):
+# 빨간색과 초록색의 차이를 느끼지 못하는 적록색약 함수문
+def RG_BFS_adj_list(graph_matrix, first_x, first_y):
     queue= deque()
     queue.append((first_x,first_y))
-    graph_matrix[first_x][first_y]=1 #방문 체크
-    count=1 # 위에서 방문체크 먼저 해줬으므로 1부터 시작.
+    visited[first_x][first_y]=1 # 방문 체크
     while queue:
         x,y = queue.popleft()
-        # 4방향 검사
-        for i in range(4):
-            nx= x+dx[i]
-            ny= y+dy[i]
-            if 0<= nx < M and 0<= ny< N:
-                if graph_matrix[nx][ny]==0:
-                    graph_matrix[nx][ny]=1 #방문체크
-                    count+=1
-                    queue.append((nx,ny))
-    return count
+        if graph_matrix[x][y]=='R' or graph_matrix[x][y]=='G':
+          # 4방향 검사
+          for i in range(4):
+              nx= x+dx[i]
+              ny= y+dy[i]
+              if 0<= nx < N and 0<= ny< N:
+                  if visited[nx][ny]==0: # 방문을 안했다면
+                    if graph_matrix[nx][ny]=='R' or graph_matrix[nx][ny]=='G':
+                        visited[nx][ny]=1 #방문체크
+                        queue.append((nx,ny))
+        else: # 'B'라는 거니까
+          #4방향 검사
+          for i in range(4):
+              nx= x+dx[i]
+              ny= y+dy[i]
+              if 0<= nx < N and 0<= ny< N:
+                  if visited[nx][ny]==0: # 방문을 안했다면
+                    if graph_matrix[nx][ny]=='B':
+                        visited[nx][ny]=1 #방문체크
+                        queue.append((nx,ny))
 
-splitAreaCount=0
-splitAreaCount_list=[]
+# 적록색약이 아닌 사람이 보는 그림
+def RGB_BFS_adj_list(graph_matrix, first_x, first_y):
+    queue= deque()
+    queue.append((first_x,first_y))
+    visited[first_x][first_y]=1 # 방문 체크
+    while queue:
+        x,y = queue.popleft()
+        if graph_matrix[x][y]=='R':
+          # 4방향 검사
+          for i in range(4):
+              nx= x+dx[i]
+              ny= y+dy[i]
+              if 0<= nx < N and 0<= ny< N:
+                  if visited[nx][ny]==0: # 방문을 안했다면
+                    if graph_matrix[nx][ny]=='R':
+                        visited[nx][ny]=1 #방문체크
+                        queue.append((nx,ny))
+        elif graph_matrix[x][y]=='B': # 'B'라면
+          #4방향 검사
+          for i in range(4):
+              nx= x+dx[i]
+              ny= y+dy[i]
+              if 0<= nx < N and 0<= ny< N:
+                  if visited[nx][ny]==0: # 방문을 안했다면
+                    if graph_matrix[nx][ny]=='B':
+                        visited[nx][ny]=1 #방문체크
+                        queue.append((nx,ny))
+        else: # 'G'
+          #4방향 검사
+          for i in range(4):
+              nx= x+dx[i]
+              ny= y+dy[i]
+              if 0<= nx < N and 0<= ny< N:
+                  if visited[nx][ny]==0: # 방문을 안했다면
+                    if graph_matrix[nx][ny]=='G':
+                        visited[nx][ny]=1 #방문체크
+                        queue.append((nx,ny))
 
-for i in range(M):
+
+# 적록색약이 보는 그림 
+visited=[[0]*(N) for _ in range(N)] # 방문체크
+count=0
+for i in range(N):
   for j in range(N):
-    if graph_matrix[i][j]==0:
-      splitAreaCount+=1
-      splitAreaCount_list.append(BFS_adj_list(graph_matrix, i, j))
+    if visited[i][j]==0:
+      count+=1
+      visited[i][j]=1
+      RGB_BFS_adj_list(graph_matrix, i, j)
+print(count, end=' ')
 
-print(splitAreaCount)
-splitAreaCount_list.sort()
-for i in range(len(splitAreaCount_list)):
-  if splitAreaCount_list[i]!=0:
-    print(splitAreaCount_list[i], end=' ')
+# 적록색약이 아닌 사람이 보는 그림
+visited=[[0]*(N) for _ in range(N)] # 방문체크
+count=0
+for i in range(N):
+  for j in range(N):
+    if visited[i][j]==0:
+      count+=1
+      visited[i][j]=1
+      RG_BFS_adj_list(graph_matrix, i, j)
+print(count)
