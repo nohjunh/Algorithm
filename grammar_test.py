b02a2fccab2fd02c9f1ef182077a8a03b6898ebe
nohjunh <nsh042777@naver.com>
2022-08-21 10:19:20 +0900

    2636 치즈

--- grammar_test.py
+++ grammar_test.py
@@ -1,101 +1,50 @@
-# 10026 적록색약 
+# 2636 치즈
 import sys
 from collections import deque
 input= sys.stdin.readline
 
-# N= 그리드 크기 
-N= int(input())
-graph_matrix = [list(input().rstrip()) for _ in range(N)]
+# N= 사각형 모양 판의 세로
+# M= 사각형 모양 판의 가로 
+N,M = map(int, input().split())
+graph_matrix = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
 # 4방향검사
 dx=[0,0,1,-1]
 dy=[1,-1,0,0]
 
-# 빨간색과 초록색의 차이를 느끼지 못하는 적록색약 함수문
-def RG_BFS_adj_list(graph_matrix, first_x, first_y):
+def BFS_adj_list(graph_matrix, first_x, first_y):
+    visited= [[False]*(M) for _ in range(N)]
     queue= deque()
     queue.append((first_x,first_y))
-    visited[first_x][first_y]=1 # 방문 체크
+    visited[first_x][first_y]= True
+    count=0
     while queue:
         x,y = queue.popleft()
-        if graph_matrix[x][y]=='R' or graph_matrix[x][y]=='G':
-          # 4방향 검사
-          for i in range(4):
-              nx= x+dx[i]
-              ny= y+dy[i]
-              if 0<= nx < N and 0<= ny< N:
-                  if visited[nx][ny]==0: # 방문을 안했다면
-                    if graph_matrix[nx][ny]=='R' or graph_matrix[nx][ny]=='G':
-                        visited[nx][ny]=1 #방문체크
-                        queue.append((nx,ny))
-        else: # 'B'라는 거니까
-          #4방향 검사
-          for i in range(4):
-              nx= x+dx[i]
-              ny= y+dy[i]
-              if 0<= nx < N and 0<= ny< N:
-                  if visited[nx][ny]==0: # 방문을 안했다면
-                    if graph_matrix[nx][ny]=='B':
-                        visited[nx][ny]=1 #방문체크
-                        queue.append((nx,ny))
+        # 4방향 검사
+        for i in range(4):
+          nx= x+dx[i]
+          ny= y+dy[i]
+          if 0<= nx < N and 0<= ny< M and visited[nx][ny]==False:
+              if graph_matrix[nx][ny]== 1: # 치즈가 있는 칸이면
+                visited[nx][ny]= True
+                graph_matrix[nx][ny]=0  # 치즈가 녹았음을 표현
+                count+=1
+              elif graph_matrix[nx][ny]== 0:
+                visited[nx][ny]= True
+                queue.append((nx,ny))
+    result.append(count)
+    return count
 
-# 적록색약이 아닌 사람이 보는 그림
-def RGB_BFS_adj_list(graph_matrix, first_x, first_y):
-    queue= deque()
-    queue.append((first_x,first_y))
-    visited[first_x][first_y]=1 # 방문 체크
-    while queue:
-        x,y = queue.popleft()
-        if graph_matrix[x][y]=='R':
-          # 4방향 검사
-          for i in range(4):
-              nx= x+dx[i]
-              ny= y+dy[i]
-              if 0<= nx < N and 0<= ny< N:
-                  if visited[nx][ny]==0: # 방문을 안했다면
-                    if graph_matrix[nx][ny]=='R':
-                        visited[nx][ny]=1 #방문체크
-                        queue.append((nx,ny))
-        elif graph_matrix[x][y]=='B': # 'B'라면
-          #4방향 검사
-          for i in range(4):
-              nx= x+dx[i]
-              ny= y+dy[i]
-              if 0<= nx < N and 0<= ny< N:
-                  if visited[nx][ny]==0: # 방문을 안했다면
-                    if graph_matrix[nx][ny]=='B':
-                        visited[nx][ny]=1 #방문체크
-                        queue.append((nx,ny))
-        else: # 'G'
-          #4방향 검사
-          for i in range(4):
-              nx= x+dx[i]
-              ny= y+dy[i]
-              if 0<= nx < N and 0<= ny< N:
-                  if visited[nx][ny]==0: # 방문을 안했다면
-                    if graph_matrix[nx][ny]=='G':
-                        visited[nx][ny]=1 #방문체크
-                        queue.append((nx,ny))
+result= []
+time_cheese=0
+# 치즈 위치가 0이면 큐에 넣어서 BFS 돌리고 
+# 1을 만날 때마다 0으로 바꿔주면서 time 증가시키면 가장자리부터 처리되는거
+while True:
+  count= BFS_adj_list(graph_matrix, 0, 0)
+  if count==0:
+    break
+  time_cheese+=1
 
-
-# 적록색약이 보는 그림 
-visited=[[0]*(N) for _ in range(N)] # 방문체크
-count=0
-for i in range(N):
-  for j in range(N):
-    if visited[i][j]==0:
-      count+=1
-      visited[i][j]=1
-      RGB_BFS_adj_list(graph_matrix, i, j)
-print(count, end=' ')
-
-# 적록색약이 아닌 사람이 보는 그림
-visited=[[0]*(N) for _ in range(N)] # 방문체크
-count=0
-for i in range(N):
-  for j in range(N):
-    if visited[i][j]==0:
-      count+=1
-      visited[i][j]=1
-      RG_BFS_adj_list(graph_matrix, i, j)
-print(count)
+print(time_cheese) 
+# 마지막은 count가 0일테니까 마지막 그 전을 출력
+print(result[len(result)-2])
