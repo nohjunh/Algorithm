c6dab0588795575f2b3f512baec6a5b9231abda1
nohjunh <nsh042777@naver.com>
2022-08-24 17:13:14 +0900

    # 16953 A->B

--- submit.py
+++ submit.py
@@ -1,28 +1,23 @@
-# 1074 Z
+# 16953 A->B
 import sys
+from collections import deque
 input= sys.stdin.readline
-# r행 c열
-N, r, c= map(int, input().split())
 
-# 2^N X 2^N => Z모양 탐색
-# N=2 => 4X4 2차원 배열 -> 2X2로 4등분
-count=0
-def solve(N, x, y): #행렬
-  global count
-  if x==r and y==c:
-    print(round(count))
-    exit()
-  if N==1: # N=1이면 => 2X2 배열이고 조건에 맞지 않다면 그 분면은 지나치는거니 +1씩
-    count+=1
-    return
-  # N=3이면 -> 8X8배열이고 -> 8/2이면->4 => 2X2배열 4개 -> x,y범위인 0~1 까지 중 r,c 없으면 그 배열 크기만큼 +해줌
-  # N=4이면 -> 16X16배열이고 -> 16/2이면-> 8 => 4X4배열 4개-> x,y범위인 0~4까지 중 r,c 없으면 그 배열 크기만큼 +해줌
-  if not (x<= r <x+N and y<= c <y+N):
-    count += N*N
-    return
-  solve(N/2, x, y) # 2사분면
-  solve(N/2, x, y+N/2) # x=행 , y=열이므로 이게 1사분면 가리킴.
-  solve(N/2, x+N/2, y) # 3사분면
-  solve(N/2, x+N/2, y+N/2) # 4사분면
-    
-solve(2**N,0,0) # N=2 -> 4X4
+# 정수 A
+# 목표 정수 B
+A, B= map(int, (input().rstrip().split()))
+
+def BFS_with_adj_list(root):
+  queue= deque([(root, 1)])
+  while queue:
+      num, count = queue.popleft()
+      if num==B:
+        print(count)
+        exit()
+      if num<=B:
+        queue.append( (num*2, count+1) )
+        queue.append( (int(str(num)+str(1)), count+1) )
+      else:
+        continue
+BFS_with_adj_list(A)
+print("-1")
