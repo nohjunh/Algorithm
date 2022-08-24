35c9906c88fb58e82b7e1db8d9a41a11cf89784a
nohjunh <nsh042777@naver.com>
2022-08-24 15:46:34 +0900

    # permutations 함수 이용

--- grammar_test.py
+++ grammar_test.py
@@ -1,18 +1,15 @@
-# 9095
+# 15663
+# permutations함수 이용
+
+import itertools
 import sys
+from itertools import permutations
 input= sys.stdin.readline
 
-T= int(input())
-DP=[0]*(11) #N의 크기는 11보다 작다.
-DP[1]=1 
-DP[2]=2
-DP[3]=4
-DP[4]=7
-for i in range(5,11):
-  DP[i]=DP[i-1]+DP[i-2]+DP[i-3]
-for i in range(T):
-  N=int(input())
-  print(DP[N])    
 
-
-  
+N, M= map(int, input().split())
+num_list=list(map(int, input().rstrip().split()))
+ans=itertools.permutations(num_list, M)
+ans_list=sorted(list((set(ans))))
+for i in ans_list:
+  print(*i)
