d77930c87d51c439185bec54b42d1bd4b454152c
nohjunh <nsh042777@naver.com>
2022-08-24 16:31:15 +0900

    # 11725 트리의 부모찾기

--- grammar_test.py
+++ grammar_test.py
@@ -1,15 +1,30 @@
-# 15663
-# permutations함수 이용
-
-import itertools
 import sys
-from itertools import permutations
+from collections import deque
 input= sys.stdin.readline
 
+# 노드의 개수 N
+N= int(input())
+graph_matrix= [[]for _ in range(N+1)]
+ans=[0]*(N+1)
 
-N, M= map(int, input().split())
-num_list=list(map(int, input().rstrip().split()))
-ans=itertools.permutations(num_list, M)
-ans_list=sorted(list((set(ans))))
-for i in ans_list:
-  print(*i)
+# 양방향 연결
+for i in range(N-1):
+    node1, node2= map(int, input().split())
+    graph_matrix[node1].append(node2)
+    graph_matrix[node2].append(node1)
+
+def BFS_with_adj_list(graph_matrix, root):
+  queue= deque([root])
+  visited.add(root)
+  while queue:
+      n = queue.popleft()
+      for neigh in graph_matrix[n]:
+          if neigh not in visited:
+            ans[neigh]=n
+            visited.add(neigh)
+            queue.append(neigh)
+
+visited = set()
+BFS_with_adj_list(graph_matrix, 1)
+for i in range(2,N+1):
+  print(ans[i])
