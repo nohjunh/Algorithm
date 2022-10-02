56973b17fdb180f426dcc4f340c27ad9f476af00
nohjunh <nsh042777@naver.com>
2022-10-02 15:42:38 +0900

    Priority queue review

--- grammar_test.py
+++ grammar_test.py
@@ -1,6 +1,38 @@
-def ccw(i, j, k):
-  area2= (j[0] - i[0]) * (k[1] - i[1]) - (j[1] - i[1]) * (k[0] - i[0])
-  if area2 > 0:
-    return True # 반시계방향 turn
-  else:
-    return False # 시계방향 turn
+#Priority queue
+
+######## 1. Priority queue unordered list ver.
+class UnorderedMinPQ:
+  def __init__(self):
+    self.pq= []
+  def insert(self, key):
+    self.pq.append(key)
+  def delMin(self):
+    minId, min = None, None
+    for idx, item in enumerate(self. pq):
+      if minId == None or item < min:
+        minId, min = idx, item
+    del self.pq[minId] #del 키워드를 통한 삭제
+    return min # 최솟값 반환
+
+# if __name__ == "__main__":
+#   pq = UnorderedMinPQ()
+#   pq.insert(3)
+#   pq.insert(4)
+#   min= pq.delMin()
+#   print(min)
+
+######### 2. Priority queue Heap ver. [ (example) 최대값만 남겨놓음 ]
+from queue import PriorityQueue
+
+def topM(input, m):
+    pq = PriorityQueue()
+    for i in input:
+        pq.put(i)
+        if pq.qsize() > m:
+            pq.get()
+    
+    return [pq.get() for _ in range(m)]
+
+if __name__ == "__main__":    
+    print(topM([52,21,50,100,-24,-6,2,26,42,77,89,0,-44],4))
+    print(topM([61,18,2,-100,-24,77],3))
