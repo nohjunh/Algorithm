c0121c7f66a79ee4b7addf9e2d9a93565ed47a66
nohjunh <nsh042777@naver.com>
2022-10-02 13:26:22 +0900

    CollinearPoints

--- submit.py
+++ submit.py
@@ -1,59 +1,45 @@
-import math
-
-def ccw(i, j, k):
-  area2= (j[0] - i[0]) * (k[1] - i[1]) - (j[1] - i[1]) * (k[0] - i[0])
-  if area2 > 0:
-    return True # 반시계방향 turn
-  else:
-    return False # 시계방향 turn
-
-def get_P_Points(testPoints):
-  testPoints= sorted(testPoints, key = lambda p: (p[1],-p[0])) # y값 기준 오름차순(y값이 제일 작은 값 찾음), x값 기준 내림차순 (x값이 제일 큰 값 찾음)
-  return (testPoints[0][0], testPoints[0][1])  ## max_X_Value= testPoints[0][0], small_Y_Value= testPoints[0][1]
-
 def angle_Cal(points, p_Value): # 점 p로부터 반시계방향으로 거쳐가는 차례로 포함
-  points= sorted(points, key= lambda x:x[1]) #########중요######### -> 같은 각도일때 탐색 순서를 맞춰주기 위함.
-  angleOrder=[]
-  radian_Set=[]
-  j=0
+  gradient_Set=[]
   for i in points:
     if i==p_Value:
-      j+=1
       continue
-    radian= math.atan2(i[1]-p_Value[1], i[0]-p_Value[0])
-    #degree= radian*180 / math.pi
-    radian_Set.append((j, radian))
-    j+=1
-  radian_Set= sorted(radian_Set, key=lambda x:x[1])
-  for i in radian_Set:
-    angleOrder.append(points[i[0]])
-  return angleOrder
+    try:
+      gradient= (i[1]-p_Value[1])/(i[0]-p_Value[0])
+      ##### 0.0이 -가 나오는데 결국 같으므로 통일 시키기 위함.
+      if gradient== -0.0:
+        gradient=0.0
+    except:
+      gradient= float('inf')
+    gradient_Set.append(gradient)
+  return gradient_Set
 
-def grahamScan(points):
-  p_Value= (get_P_Points(points))
-  hull=[]
-  hull.append(p_Value)
-  angleOrder= angle_Cal(points, p_Value)
-  angleOrder.append(p_Value) # 마지막 p_Value랑 연결해서 판단하게 하기위함
-  for k in angleOrder:
-    while len(hull) >=2:
-      # stack기능이라고 생각해야되므로 append시 맨 뒤에 붙는거 고려.
-      # 맨 뒤에서부터 한 두 개씩 가져온다.
-      i,j = hull[-2], hull[-1] # [-1]: first, [-2]: second
-      if ccw(i,j,k): 
-        break # true이면 second값 hull에 그대로 있으면 됨.
-      hull.pop() # false면 pop해서 second값 빼줌
-    hull.append(k)
-  hull.pop(-1) # 마지막 하나 추가한 p_Value가 hull에 들어가기에 빼줌(중복값)
-  return hull
-
+def collinearPoints(points):
+  ans=[]
+  points= sorted(points, key=lambda p:(p[0],p[1]))
+  gradient_collection=[]
+  for i in points:
+    gradient_Set= angle_Cal(points, i)
+    counter = {}
+    for value in gradient_Set:
+      if value in counter:
+          counter[value] += 1
+      else:
+          counter[value] = 1
+    for key, value in counter.items(): # key=기울기, value= 그 기울기가 몇 개가 있는지
+      if value>=3:
+        if (key,i[0],i[1]) in gradient_collection:
+          continue
+        else:
+          index=0 # 마지막 요소의 인덱스값을 저장하기 위함.
+          for j in enumerate(gradient_Set):
+            if j[1]==key:
+              index= j[0]
+              gradient_collection.append((key, points[index+1][0],points[index+1][1]))
+          ans.append((i[0],i[1], points[index+1][0],points[index+1][1]))
+  return ans
 
 if __name__ == "__main__":
-  # 백준 test_case
-   hull=[]
-   N=int(input())
-   for i in range(N):
-     a,b=map(int,input().split())
-     hull.append((a,b))
-   hull= grahamScan(hull)
-   print(len(hull))
+  print(collinearPoints([(0,0), (3,1), (2,3), (4,4), (6,6), (7,7), (9,9)]))
+  print(collinearPoints([(1,0), (2,0), (3,0), (4,0), (5,0), (6,0), (8,0)]))
+  print(collinearPoints([(7,0), (14,0), (22,0), (27,0), (31,0), (42,0)]))
+  print(collinearPoints([(1,1), (2,2), (3,3), (4,4), (2,0), (3,-1), (4,-2), (0,1), (-1,1), (-2,1), (-3,1), (2,1), (3,1), (4,1), (5,1)]))
