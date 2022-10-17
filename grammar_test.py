f1936d7e5d25498406ed8d80e4d90dc96a0e30a4
nohjunh <nsh042777@naver.com>
2022-10-17 15:40:01 +0900

    # Bottom-up Merge sort

--- grammar_test.py
+++ grammar_test.py
@@ -1,38 +1,38 @@
-#Priority queue
+# Top-down merge-sort는 함수 호출/ 반환 과정에서 함수 인자, 지역변수 등을
+# 메모리에 저장/ 삭제를 하는 과정을 반복하게 된다.
+# 따라서, 이러한 과정을 통한 시간/공간 Overhead가 크게 된다.
+# Bottom-up Merge Sort를 통해 Divide를 위한 재귀 호출 과정을 생략하여 Merge로만 sorting이 되도록 진행한다.
+# sz=1에서 시작
+# 크기 sz인 인접한 부분집합끼리 병합 -> sz 2배하고 sz>=N이 될 때까지 반복
 
-######## 1. Priority queue unordered list ver.
-class UnorderedMinPQ:
-  def __init__(self):
-    self.pq= []
-  def insert(self, key):
-    self.pq.append(key)
-  def delMin(self):
-    minId, min = None, None
-    for idx, item in enumerate(self. pq):
-      if minId == None or item < min:
-        minId, min = idx, item
-    del self.pq[minId] #del 키워드를 통한 삭제
-    return min # 최솟값 반환
+# Copyright by Sihuing Lee
 
-# if __name__ == "__main__":
-#   pq = UnorderedMinPQ()
-#   pq.insert(3)
-#   pq.insert(4)
-#   min= pq.delMin()
-#   print(min)
 
-######### 2. Priority queue Heap ver. [ (example) 최대값만 남겨놓음 ]
-from queue import PriorityQueue
-
-def topM(input, m):
-    pq = PriorityQueue()
-    for i in input:
-        pq.put(i)
-        if pq.qsize() > m:
-            pq.get()
+def merge(a, aux, lo, mid, hi):
+    for k in range(lo, hi+1):
+        aux[k] = a[k] # 예비 배열에 a[k]를 옮김 -> a[k]를 결과배열로 쓰기 위함.
+        # 단, 어차피 배열을 인자로 전달하는 것이므로 주소를 복사할 것임. 즉, 새로 배열을 만드는 과정이 아니라
+        # 정렬 결과를 바로바로 a[]에 적용시키는 것임.
     
-    return [pq.get() for _ in range(m)]
+    i, j = lo, mid+1
+    for k in range(lo, hi+1):
+        if i>mid: a[k], j = aux[j], j+1            
+        elif j>hi: a[k], i = aux[i], i+1            
+        elif aux[i] <= aux[j]: a[k], i = aux[i], i+1
+        else: a[k], j = aux[j], j+1            
 
-if __name__ == "__main__":    
-    print(topM([52,21,50,100,-24,-6,2,26,42,77,89,0,-44],4))
-    print(topM([61,18,2,-100,-24,77],3))
+    return a
+
+def mergeSort(a):
+    aux = [None] * len(a)
+
+    sz = 1
+    while( sz < len(a) ):
+        for lo in range(0, len(a)-sz, sz*2):
+            merge(a, aux, lo, lo+sz-1, min(lo+sz+sz-1, len(a)-1))            
+        sz += sz  # 2배 -> 2배 -> 2배 ...
+
+    return a
+
+if __name__ == "__main__":
+    print(mergeSort([10, 9, 3, 2, 6, 7]))
