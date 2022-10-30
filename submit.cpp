5b4d4140ab98a95b61b7ed7e0a848e0e0179c706
nohjunh <nsh042777@naver.com>
2022-10-30 19:49:43 +0900

    #1021_회전하는 큐

--- submit.cpp
+++ submit.cpp
@@ -1,41 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stack>
+#include <string>
+#include <deque>
 #include <queue>
+#include <vector>
+#include <sstream>
 using namespace std;
 
 int main() {
-	int TestCase;
-	int N, M, value;
-	int countPrint = 0;
+	deque<int> dq;
+	int N, M;
+	cin >> N >> M;
+	int left = 0;
+	int right = 0;
+	int cnt = 0;
 
-	cin >> TestCase;
-
-	for (int k = 0; k < TestCase; k++) {
-		countPrint = 0;
-		cin >> N >> M;
-		queue<pair<int, int>> q;
-		priority_queue<int> PQ;
-		for (int i = 0; i < N; i++) {
-			cin >> value;
-			q.push(make_pair(i, value));
-			PQ.push(value);
+	for (int i = 1; i <= N; i++) {
+		dq.push_back(i);
+	}
+	for (int i = 0; i < M; i++) {
+		int index;
+		cin >> index;
+		for (int i = 0; dq.size(); i++) {
+			if (dq[i] == index) {
+				left = i;
+				right = dq.size() - i;
+				break;
+			}a
 		}
-		while (!q.empty()) {
-			int index = q.front().first;
-			int value = q.front().second;
-			if (PQ.top() != value) {
-				q.push(q.front());
-				q.pop();
-			}
-			else {
-				q.pop();
-				PQ.pop();
-				countPrint++; // 몇번 출력 됐는지를 나타내는 변수 +1
-				if (index == M) {
-					cout << countPrint << endl;
+		if (left <= right) { // 왼쪽 원소를 오른쪽으로 이동시키는게 더 낫다
+			while (1) {
+				if (dq.front() == index) {
 					break;
 				}
+				dq.push_back(dq.front());
+				dq.pop_front();
+				cnt++;
 			}
+			dq.pop_front(); // 맨 왼쪽을 뽑으면 된다.
+		}
+		else { // left가 right보다 더 길기에 오른쪽 원소를 왼쪽으로 이동시키는게 더 낫다
+			cnt++;
+			while (1) {
+				if (dq.back() == index) {
+					break;
+				}
+				dq.push_front(dq.back());
+				dq.pop_back();
+				cnt++;
+			}
+			dq.pop_back(); // 맨 뒤를 뽑으면 된다.
 		}
 	}
+	cout << cnt << endl;
+
 	return 0;
 }
