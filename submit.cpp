f68bbed3d70b81209020d0f6523a12b9ba47c7a1
nohjunh <nsh042777@naver.com>
2022-10-30 19:51:59 +0900

    #1874_스택수열

--- submit.cpp
+++ submit.cpp
@@ -1,54 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stack>
+#include <string>
+#include <deque>
+#include <queue>
+#include <vector>
+#include <sstream>
 using namespace std;
 
-#define MAX 500
+int main() {
+	stack<int> s;
+	vector<char> v;
+	int n;
+	cin >> n;
+	int cur = 1;
 
-int multiply(int x, int res[], int res_size);
+	for (int i = 0; i < n; i++) {
+		int input;
+		cin >> input;
 
-void factorial(int n)
-{
-   int result[MAX]; // 계산 결과 1자릿수를 계속 저장해줄 배열
+		while (cur <= input) { // input값까지 오름차순으로 받음.
+			s.push(cur); // 오름차순으로 들어가야하기에 오름차순으로 넣어줌.
+			v.push_back('+');
+			cur++;
+		}
+		if (s.top() == input) { // input값에 해당하면 pop시킴.
+			s.pop();
+			v.push_back('-');
+		}
+		else { // top이 input가 같지 않다면 불가능한 것.
+			cout << "NO" << "\n";
+			return 0;
+		}
+	}
 
-   result[0] = 1; // 시작은 1부터 
-   int result_size = 1; // 1부터 시작이니 당연히 result_size도 1부터
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << "\n";
+	}
 
-   for (int x = 2; x <= n; x++) // n!= 1*2...*n
-       // result값은 배열에 각 자리수마다 저장될 것
-       // result_size는 매 multiply마다 자릿수에 따라 수 업데이트 시켜야 함.
-       result_size = multiply(x, result, result_size);
-
-      // 계산결과 총 몇자리가 되는지 result_size로 리턴 받음    
-
-   for (int i = result_size -1;  i >= 0;  i--) // 맨뒤부터 저장되므로 반대로 출력하기 위함.
-       cout << result[i];
-}
-
-int multiply(int x, int result[], int result_size){
-   int carry = 0; // Initialize carry
-
-   for (int i = 0; i < result_size; i++) {
-       int prod = result[i] * x + carry; // 자리올림 수까지 더해야 함.
-
-       result[i] = prod % 10; // 자리 올림하고 남은 수가 당연히 밑에 내려올 것이다.
-
-       carry = prod / 10; // 자리 올림 수
-   }
-
-   // 연산이 모두 끝난후 result_size를 넘어간 carry가 있다면
-   // 그 carry를 처리해주기 위함.
-   while (carry) {
-       result[result_size] = carry % 10;
-       carry = carry / 10;
-       result_size++;
-   }
-   return result_size;
-}
-
-int main()
-{
-   int n;
-   cin >> n;
-
-   factorial(n);
-   return 0;
+	return 0;
 }
