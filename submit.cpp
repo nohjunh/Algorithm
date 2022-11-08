4cad444e599c03832d6c28ea598b2d2538f6699c
nohjunh <nsh042777@naver.com>
2022-11-08 23:18:07 +0900

    #1655 가운데를 말해요 C++ code

--- submit.cpp
+++ submit.cpp
@@ -1,27 +1,53 @@
-// Hashing
+// #1655 가운데를 말해요
+// 아이디어 참고: https://www.crocus.co.kr/625
+
+// 중간값(가운데값) 구하기 알고리즘
+// 1. 최대 힙의 크기는 최소 힙의 크기와 같거나, 하나 더 크다.
+// 2. 최대 힙의 최대 원소는 최소 힙의 최소 원소보다 작거나 같다.
+//	  이때 조건에 맞지 않다면 최대 힙, 최소 힙의 root값을 swap 해준다.
+// ** 위 1,2번 규칙을 모두 만족하도록 유지한다면 항상 최대 힙 root값이 중간값이 됨.
+
 #include <iostream>
-#include <string>
+#include <queue>
 using namespace std;
-const int M = 1234567891; // M의 값은 1234567891로 고정하자.
-string str;
-int L;
-
-long long Hashing(string str) {
-	long long hash = 0;
-	long long r = 1; // 거듭제곱의 값인 r변수. r의 값은 31로 고정, 31^1 -> 31^2 ...
- 
-	for (int i = 0; i < L; i++) { // 문자열의 길이만큼 본다.
-		hash = (hash + (str[i] - 'a' + 1) * r) % M; //str[i]-'a'을 해줌으로써 해당 알파벳의 int값을 대응
-		r = (r * 31) % M; // 지수 증가 // 값이 너무 커지니까 유한한 출력을 가질 수 있도록 계속 mod M
-	}
-
-	return hash;
-}
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	// 동적으로 요소가 들어오더라도 그 요소의 order를 유지시키기 위해선
+	// Priority_queue를 사용한다.
 
-	cin >> L; // 문자열의 길이 L이 들어온다.
-	cin >> str; // 영문 소문자로만 이루어진 문자열이 들어온다.
-	cout << Hashing(str)<<endl;
+	priority_queue<int> max_heap; // Max_Heap
+	priority_queue<int, vector<int>, greater<int>> min_heap; // Min_Heap
+	int N;
+	cin >> N; // 외치는 정수의 갯수
+
+	for (int s = 0; s < N; s++) {
+		int input;
+		cin >> input;
+
+		if (max_heap.size() == min_heap.size()){
+			max_heap.push(input);
+		}
+		else {  // 같지않다면, max_heap이 min_heap보다 하나 더 많은 상태이므로,
+			min_heap.push(input);  // min_heap에 넣어줘서 1번 조건을 맞춰준다.
+		}
+		// 2번 조건에 맞지 않다면, 즉 최대힙의 root가 최소힙의 root보다 크다면
+		if (!max_heap.empty() && !min_heap.empty()) {
+			if (max_heap.top() > min_heap.top()) {
+				// swap을 진행
+				int max_value, min_value;
+				max_value = max_heap.top();
+				min_value = min_heap.top();
+				max_heap.pop();
+				min_heap.pop();
+				max_heap.push(min_value);
+				min_heap.push(max_value);
+			}
+		}
+		cout << max_heap.top() << "\n"; // max_heap의 root가 중간값이 된다.
+	}
+
 	return 0;
 }
