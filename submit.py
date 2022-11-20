24a41954bd40cede74a0c16f2cdf58a282c75cc1
nohjunh <nsh042777@naver.com>
2022-11-20 21:46:44 +0900

    #5430 AC

--- submit.py
+++ submit.py
@@ -1,28 +1,37 @@
-# copyright sihyunglee@knu.ac.kr
-def mstPrimEager(g):
-    def include(w):
-      included[w] = True
-      for e in g.adj[w]: # w에 인접한 각 각선 w-x에 대해
-        x=e.other(w)
-        if not included[x]:
-          if not pq.contains(x):
-            pq.insert(x, e) # index, key
-          else:
-            if pq.keyOf(x).weight > e.weight:
-              pq.decreaseKey(x, e)
+# 5430 AC
+import re
+import sys
+from collections import deque
 
-    assert(isinstance(g, WUGraph))
+T= int(sys.stdin.readline())
+for _ in range(T):
+  reverse= False
+  check= False
 
-    edgesInMST = [] # Stores edges selected as part of the MST
-    included = [False] * g.V # included[v] == True if v is in the MST
-    weightSum = 0  # Sum of edge weights in the MST    
-    pq = IndexMinPQ(g.V) # Build a IndexMinPQ
-    include(0) # include(0) 호출해 정점 0 에 인접한 정점을 모두 pq에 추가함
-
-    while len(edgesInMST) < g.V-1:
-      e, w = pq.delMin() # key, index
-      edgesInMST.append(e)
-      weightSum += e.weight
-      include(w) # Add to the MST the vertex not yet included
-
-    return edgesInMST, weightSum # EdgesInMST, Weight
+  inputFunction = sys.stdin.readline()
+  n = int(sys.stdin.readline())
+  inputNumber= sys.stdin.readline().rstrip()[1:-1].split(',')
+  if n==0:
+    inputNumber= deque()
+  dQueue = deque(inputNumber)
+  for f in inputFunction:
+    if f=='R':
+      if reverse:
+        reverse= False
+      else:
+        reverse= True
+    elif f=='D':
+      if dQueue:
+       if reverse==True:
+         dQueue.pop()
+       else:
+         dQueue.popleft()
+      else:
+        check=True
+        print("error")
+        break
+  if(reverse==True and check==False):
+    dQueue.reverse()
+    print('['+ ",".join(dQueue) + ']')
+  if(reverse==False and check==False):
+    print('['+ ",".join(dQueue) + ']')
