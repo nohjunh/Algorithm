9c998530953298d518c7ef61640cbc5bc93c4ee9
nohjunh <nsh042777@naver.com>
2022-12-19 22:09:27 +0900

    #1932 정수 삼각형

--- submit.py
+++ submit.py
@@ -1,17 +1,18 @@
-def grahamScan(points):
-  p_Value= (get_P_Points(points))
-  hull=[]
-  hull.append(p_Value)
-  angleOrder= angle_Cal(points, p_Value)
-  angleOrder.append(p_Value) # 마지막 p_Value랑 연결해서 판단하게 하기위함
-  for k in angleOrder:
-    while len(hull) >=2:
-      # stack기능이라고 생각해야되므로 append시 맨 뒤에 붙는거 고려.
-      # 맨 뒤에서부터 한 두 개씩 가져온다.
-      i, j = hull[-2], hull[-1] # [-1]: 맨 뒤 기준 first가 j에 들어간다., [-2]: 맨 뒤 기준 second가 i에 들어간다.
-      if ccw(i,j,k):
-        break # true이면 convex라는 뜻이므로 second값 hull에 그대로 있으면 되고, k값 hull에 push해주면 됨.
-      hull.pop() # false면 concave라는 뜻이므로 pop해서 second값(즉, j값) 빼줌
-    hull.append(k)
-  hull.pop(-1) # 마지막 하나 추가한 p_Value가 hull에 들어가기에 빼줌(중복값)
-  return hull
+import sys
+input= sys.stdin.readline
+
+N= int(input())
+data= []
+for _ in range(N):
+  a= list(map(int, input().split()))
+  data.append(a)
+
+for i in range(1, N):
+  for j in range(len(data[i])):
+    if j==0:
+      data[i][j]= data[i][j]+ data[i-1][j]
+    elif j==len(data[i])-1:
+      data[i][j]= data[i][j]+ data[i-1][j-1]
+    else:
+      data[i][j] = max(data[i-1][j], data[i-1][j-1]) + data[i][j]
+print(max(data[N-1]))
