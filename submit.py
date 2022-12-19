60221c8315d92bcfff8e75f0db195c20a72f4adb
nohjunh <nsh042777@naver.com>
2022-12-19 15:45:19 +0900

    # grahamScan

--- submit.py
+++ submit.py
@@ -1,19 +1,17 @@
-# Image Augmentation using Numpy
-# https://www.kaggle.com/code/ichigoku/image-augmentation-using-numpy
-
-import numpy as np
-
-image_path = ""
-im = Image.open(image_path)
-image = np.array(im)
-
-# filipud
-new_img = np.flipud(image)
-
-# rotation
-new_img = np.rot90(image)
-
-# roll
-new_img = np.roll(image, 1, axis=0)
-new_img = np.roll(image, 2, axis=1)
-
+def grahamScan(points):
+  p_Value= (get_P_Points(points))
+  hull=[]
+  hull.append(p_Value)
+  angleOrder= angle_Cal(points, p_Value)
+  angleOrder.append(p_Value) # 마지막 p_Value랑 연결해서 판단하게 하기위함
+  for k in angleOrder:
+    while len(hull) >=2:
+      # stack기능이라고 생각해야되므로 append시 맨 뒤에 붙는거 고려.
+      # 맨 뒤에서부터 한 두 개씩 가져온다.
+      i, j = hull[-2], hull[-1] # [-1]: 맨 뒤 기준 first가 j에 들어간다., [-2]: 맨 뒤 기준 second가 i에 들어간다.
+      if ccw(i,j,k):
+        break # true이면 convex라는 뜻이므로 second값 hull에 그대로 있으면 되고, k값 hull에 push해주면 됨.
+      hull.pop() # false면 concave라는 뜻이므로 pop해서 second값(즉, j값) 빼줌
+    hull.append(k)
+  hull.pop(-1) # 마지막 하나 추가한 p_Value가 hull에 들어가기에 빼줌(중복값)
+  return hull
