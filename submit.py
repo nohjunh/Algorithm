3c7ea6ca47aca14d3d85528d5cbfafb0e58a2bad
nohjunh <nsh042777@naver.com>
2022-12-23 15:24:22 +0900

    #11657 타임머신

--- submit.py
+++ submit.py
@@ -1,43 +1,39 @@
-# 1707 Bipartite Graph
+# 11657 타임머신, bellman_ford algorithm
 import sys
-import queue
 input = sys.stdin.readline
-sys.setrecursionlimit(20000)
+INF = float('inf')
 
-def BFS(s):
-  q = queue.Queue()
-  q.put(s)
-  color[s]= 1
-  while not q.empty():
-    u = q.get()
-    for v in graph[u]:
-      if color[v] == 0:
-        if color[u] == 1:
-          color[v] = 2
-        else:
-          color[v] = 1
-        q.put(v)
-      elif color[v] == color[u]:
-        return False
+def bellman_ford(graph, start):
+  distTo[start] = 0
+
+  # 정점 개수만큼 반복
+  for i in range(N):
+    for v in range(1, N+1):
+      for w, weight in graph[v]:
+        if distTo[v] != INF and distTo[w] > distTo[v] + weight:
+          distTo[w] = distTo[v] + weight
+          edgeTo[w] = v
+          if i == N-1: # 값이 갱신될 수 있다면 음의 순환 존재
+            return False
   return True
+          
 
+N, M = map(int, input().split())
+graph= [[] for _ in range(N+1)]
+distTo= [INF] * (N+1)
+edgeTo= [None] * (N+1)
 
-answer= True
-K= int(input())
-for _ in range(K):
-  V, E = map(int, input().split())
-  graph = [[] for _ in range(V+1)]
-  color = [0 for _ in range(V+1)] # 0이면 아직 방문 안한거, 1이면 A그룹, 2이면 B그룹
-  for _ in range(E):
-    a,b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-  for i in range(1, V+1):
-    if color[i] == 0:
-      answer = BFS(i)
-      if not answer:
-        break
-  if answer==True:
-    print("YES")
-  else:
-    print("NO")
+for _ in range(M):
+  A,B,C = map(int, input().split())
+  graph[A].append((B,C))
+
+result = bellman_ford(graph, 1)
+
+if result == False:
+  print(-1)
+else:
+  for i in range(2, N+1):
+    if distTo[i] == INF:
+      print(-1)
+    else:
+      print(distTo[i])
