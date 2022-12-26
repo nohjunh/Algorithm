cdaf58ed43e6aaccd9bccf45a205c879343da677
nohjunh <nsh042777@naver.com>
2022-12-27 08:31:26 +0900

    # 2565 전깃줄

--- submit.py
+++ submit.py
@@ -1,13 +1,15 @@
-# 11053 가장 긴 증가하는 부분 수열
+# 2565 전깃줄
 from sys import stdin
 input = stdin.readline
 
 N= int(input())
-arr= list(map(int, input().split()))
-DP = [1 for _ in range(N)]
+arr = [list(map(int,input().split())) for _ in range(N)]
+DP = [0 for _ in range(N+1)]
+arr.sort(key = lambda x: x[0])
 
 for i in range(N):
   for j in range(i):
-    if arr[i] > arr[j]:
-      DP[i]= max(DP[i], DP[j]+1)
-print(max(DP))
+    if arr[i][1] > arr[j][1] and DP[i] < DP[j]:
+      DP[i] = DP[j]
+  DP[i] += 1
+print(N-max(DP))
