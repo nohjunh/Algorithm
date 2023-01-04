ae027c4130de0d0abf0228f4b7610cb6ab4a3837
nohjunh <nsh042777@naver.com>
2023-01-04 16:57:59 +0900

    #2170 선 긋기

--- submit.py
+++ submit.py
@@ -1,35 +1,26 @@
-# 1929 소수 구하기
-# 에라토스테네스의 체
-"""
-만약, 12가 primeNumber인지 확인하고 싶다면
-12의 약수 : 1 2 3 4 6 12
-
-1 * 12
-2 * 6
-3 * 4
---대칭--
-4 * 3
-6 * 2
-12 * 1
-이므로 2,3 만 12%2,3==0 인지 여부 판단해주면 됨.
-2,3 에서 0이 나온다면 4,6에서도 마찬가지로 0이 나올 것
-"""
-
+# 2170 선 긋기
 from sys import stdin
 input = stdin.readline
 
-def isPrime(number):
-  i= number
-  sqrt = int(i**(1/2))
-  for j in range(2, sqrt+1): # 2부터 제곱근까지 나눠봄
-    if i % j == 0:
-      return False
-  return True
+"""
+1 2 3
+  2 3 4 5
+    3 4 5
+          6 7
+"""
 
-M, N = map(int, input().split())
-for i in range(M, N+1): # M부터 N까지
-  if i==1:
-    continue
+pointSet= [list(map(int, input().split())) for _ in range(int(input()))]
+pointSet.sort(key = lambda x:x[0])
+tracePointX, tracePointY= pointSet[0]
+result= 0
+
+for idx in range(1, len(pointSet)):
+  curPointX, curPointY = pointSet[idx]
+  if tracePointY < curPointX:
+    result += tracePointY-tracePointX
+    tracePointX, tracePointY = pointSet[idx]
   else:
-    if isPrime(i):
-      print(i)
+    tracePointY = tracePointY if tracePointY > curPointY else curPointY
+result += tracePointY-tracePointX
+
+print(result)
